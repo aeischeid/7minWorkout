daf0beb1bea9d87b7626521fb8230e5566242d75
aeischeid <a.eischeid@gmail.com>
2013-07-07 11:02:41 -0500

    initial commit - most logic working, still pretty bare bones

---
+++ main.js
@@ -0,0 +1,67 @@
+var excercises = ['jumping jacks', 'push ups', 'chair step ups', 'sit ups', 'wall sits', 'plank', 'side plank' ]
+var excerciseDurration = 6
+var breakDurration = 3
+var workoutEl = document.getElementById('workout')
+var landingEl = document.getElementById('landing')
+
+var doWorkout = function (){
+	console.log('startWorkout')
+	var workoutTimer, excerciseTimer, currentExcercise, timeEl
+	currentExcercise = 0
+	// hide the landing view and show the workout view
+	landingEl.style.left = '-100%'
+	workoutEl.style.right = 0
+	// some DOM elements that will be accessed a lot
+	timeEl = document.getElementById('individualTimer')
+	excerciseName = document.getElementById('excerciseName')
+	// this is where the magic happens
+	var doExcersie = function(count, breakCount){
+		var breakTime = false
+		excerciseName.textContent = excercises[currentExcercise]
+		// an abstracted timer tick down func
+		var timerDown = function() {
+			timeEl.textContent = count
+			count--
+		}
+		//the first interval doesn't execute until after 1 sec so kick off the first timer tick manually
+		timerDown()
+		var counter = setInterval(function(){
+			if(count > -1) {
+				timerDown()
+			} else {
+				if(!breakTime) {
+					breakTime = true
+					currentExcercise++
+					count = breakCount
+					timerDown()
+					excerciseName.textContent = excercises[currentExcercise] + ' in' 
+					// advance the master progressBar, change the image
+					workoutEl.querySelector('#progressBar p:nth-child('+currentExcercise+')').textContent = 'x'
+				} else {
+					//console.log('should be clearing interval... ', this)
+					clearInterval(counter)
+					if(currentExcercise < excercises.length) {
+						doExcersie(excerciseDurration, breakDurration) // happening 1 sec to early...
+					} else {
+						endWorkout()
+					}
+				}
+			}
+		}, 1000)
+	}
+	
+	doExcersie(excerciseDurration, breakDurration) // calls recursively until currentExcercise < excercises.length
+}
+
+var endWorkout = function(){
+	console.log('endWorkout')
+	landingEl.style.left = '0'
+	workoutEl.style.right = '-100%'
+}
+
+var pauseWorkout = function(){
+	console.log('pauseWorkout -doesnt work yet')
+	landingEl.style.left = '0'
+	workoutEl.style.right = '-100%'
+}
+
